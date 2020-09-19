cff81e3067b324a2602612c0f605b6919baab300
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-09-19 13:17:21 -0300

    Add bitmap font creator class

---
+++ BitmapFontCreator/FontBuilder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace BitmapFontCreator
+{
+    class FontBuilder
+    {
+        public int LeftPadding { get; set; }
+        public int RightPadding { get; set; }
+        public int TopPadding { get; set; }
+        public int BottomPadding { get; set; }
+        public int Size { get; set; }
+
+        private readonly String fontFamily;
+        private Font font;
+
+        public FontBuilder(String fontFamily)
+        {
+            this.fontFamily = fontFamily;
+            Size = 16;
+        }
+
+        public void Build()
+        {
+            font = new Font(fontFamily, Size, FontStyle.Regular);
+            String symbols = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~€™¡¢£¥©ª«®²³¹º»¿ÀÁÂÃÄÇÈÉÊËÍÎÏÑÓÔÕÚÛÜàáâãçèéêëíîïñóôõ÷úûü";
+            System.IO.Directory.CreateDirectory(font.Name);
+            foreach (char c in symbols)
+            {
+                Image image = CreateCharImage(c, font);
+                String name = String.Format("C{0:X}.bmp", (int)c);
+                name = System.IO.Path.Combine(font.Name, name);
+                image.Save(name, ImageFormat.Bmp);
+                image.Dispose();
+            }
+        }
+
+        private SizeF MesureTile(String text, Font font, StringFormat stringFormat)
+        {
+            Image image = new Bitmap(1, 1, PixelFormat.Format24bppRgb);
+            Graphics drawing = Graphics.FromImage(image);
+            SizeF textSize = drawing.MeasureString(text, font, 100, stringFormat);
+            image.Dispose();
+            drawing.Dispose();
+            textSize.Width += LeftPadding +  RightPadding;
+            textSize.Height += TopPadding + BottomPadding;
+            return textSize;
+        }
+
+        private Image CreateCharImage(char c, Font font)
+        {
+            String text = c.ToString();
+            StringFormat stringFormat = new StringFormat(StringFormat.GenericTypographic);
+            SizeF textSize;
+            if (c == ' ') textSize = MesureTile("i", font, stringFormat);
+            else textSize = MesureTile(text, font, stringFormat);
+            Image image = new Bitmap((int)textSize.Width, (int)textSize.Height, PixelFormat.Format24bppRgb);
+            Graphics drawing = Graphics.FromImage(image);
+            drawing.Clear(Color.White);
+            Brush textBrush = new SolidBrush(Color.Black);
+            stringFormat.Alignment = StringAlignment.Near;
+            RectangleF rect = new RectangleF(LeftPadding, TopPadding, textSize.Width, textSize.Height);
+            drawing.DrawString(text, font, Brushes.Black, rect, stringFormat);
+            textBrush.Dispose();
+            drawing.Dispose();
+            return image;
+        }
+    }
+}
--- BitmapFontCreator/Program.cs
+++ BitmapFontCreator/Program.cs
@@ -1,83 +1,20 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
 
 namespace BitmapFontCreator
 {
     class Program
     {
-        private static SizeF MesureTile(String text, Font font, StringFormat stringFormat, int horizontalPadding, int verticalPadding)
-        {
-            Image image = new Bitmap(1, 1, PixelFormat.Format24bppRgb);
-            Graphics drawing = Graphics.FromImage(image);
-            SizeF textSize = drawing.MeasureString(text, font, 100, stringFormat);
-            image.Dispose();
-            drawing.Dispose();
-            textSize.Width += horizontalPadding * 2;
-            textSize.Height += verticalPadding;
-            return textSize;
-        }
-
-        private static Image CreateCharImage(char c, String fontName, int size, int horizontalPadding, int verticalPadding)
-        {
-            String text = c.ToString();
-            StringFormat stringFormat = new StringFormat(StringFormat.GenericTypographic);
-            Font font = new Font(fontName, size, FontStyle.Regular);
-            SizeF textSize = MesureTile(text, font, stringFormat, horizontalPadding, verticalPadding);
-            Image image = new Bitmap((int)textSize.Width, (int)textSize.Height, PixelFormat.Format24bppRgb);
-            Graphics drawing = Graphics.FromImage(image);
-            drawing.Clear(Color.White);
-            Brush textBrush = new SolidBrush(Color.Black);
-            stringFormat.Alignment = StringAlignment.Center;
-            RectangleF rect = new RectangleF(0, 0, textSize.Width, textSize.Height);
-            drawing.DrawString(text, font, Brushes.Black, rect, stringFormat);
-            textBrush.Dispose();
-            drawing.Dispose();
-            return image;
-        }
-
-        private static Image CreateSpaceImage(String fontName, int size, int horizontalPadding, int verticalPadding)
-        {
-            String text = "i";
-            StringFormat stringFormat = new StringFormat(StringFormat.GenericTypographic);
-            Font font = new Font(fontName, size, FontStyle.Regular);
-            SizeF textSize = MesureTile(text, font, stringFormat, horizontalPadding, verticalPadding);
-            Image image = new Bitmap((int)textSize.Width, (int)textSize.Height, PixelFormat.Format24bppRgb);
-            Graphics drawing = Graphics.FromImage(image);
-            drawing.Clear(Color.White);
-            drawing.Dispose();
-            return image;
-        }
-
         static void Main(string[] args)
         {
-            string fontName = "Arial";
-            int size = 16;
-            int horizontalPadding = 1;
-            int verticalPadding = 0;
-
+            string fontName = "Times";
             if (args.Length > 0) fontName = args[0];
-            if (args.Length > 1) size = int.Parse(args[1]);
-            if (args.Length > 2) horizontalPadding = int.Parse(args[2]);
-            if (args.Length > 3) verticalPadding = int.Parse(args[3]);
+            FontBuilder fontBuilder = new FontBuilder(fontName);
 
-            System.IO.Directory.CreateDirectory(fontName);
+            if (args.Length > 1) fontBuilder.Size = int.Parse(args[1]);
+            if (args.Length > 2) fontBuilder.LeftPadding = int.Parse(args[2]);
+            if (args.Length > 3) fontBuilder.TopPadding = int.Parse(args[3]);
 
-            String name = System.IO.Path.Combine(fontName, "C20.bmp");
-            Image image = CreateSpaceImage(fontName, size, horizontalPadding, verticalPadding);
-            image.Save(name, ImageFormat.Bmp);
-            image.Dispose();
-
-            String symbols = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~€™¡¢£¥©ª«®²³¹º»¿ÀÁÂÃÄÇÈÉÊËÍÎÏÑÓÔÕÚÛÜàáâãçèéêëíîïñóôõ÷úûü";
-
-            foreach (char c in symbols)
-            {
-                image = CreateCharImage((char)c, fontName, size, horizontalPadding, verticalPadding);
-                name = String.Format("C{0:X}.bmp", (int)c);
-                name = System.IO.Path.Combine(fontName, name);
-                image.Save(name, ImageFormat.Bmp);
-                image.Dispose();
-            }
+            fontBuilder.Build();
         }
     }
 }
